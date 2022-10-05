72dcd510ae1c006d9ecf4d2fcd50ded7c020828b
xHMZx <marcusdamon48@gmail.com>
2022-10-05 19:29:38 +0500

    Issues while implementing half scrollable Landscape view UI

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -367,157 +367,157 @@ class _DashBoardState extends State<DashBoard> {
           Row(
 
             children: [
-              // Expanded(
-              //   flex: 3,
-              //   child: Container(
-              //     child: SingleChildScrollView(
-              //       child: Column(
-              //
-              //           children: [
-              //             Container(
-              //               color: Colors.black12,
-              //               padding: EdgeInsets.symmetric(vertical: 3),
-              //               height: 50,
-              //               child: Center(
-              //                 child: ListView.builder(
-              //                   scrollDirection: Axis.horizontal,
-              //                   itemCount: navBarItem.length,
-              //                   itemBuilder: (context ,index ){
-              //                     return InkWell(
-              //                       onTap: (){
-              //                         if (navBarItem[index]!= "Home")
-              //                         {
-              //                           Navigator.push(context, MaterialPageRoute(builder: (context) => CategoryPage(query: navBarItem[index])));
-              //                         }
-              //                       },
-              //                       child: Container(
-              //
-              //                         decoration: BoxDecoration(
-              //                           borderRadius: BorderRadius.circular(14),
-              //                           color: Colors.blue.shade900,
-              //
-              //                         ),
-              //                         padding: EdgeInsets.symmetric(horizontal: 20),
-              //
-              //                         margin: EdgeInsets.symmetric(horizontal: 7),
-              //                         child: Center
-              //                           (
-              //                             child: Text(navBarItem[index],
-              //                               style: TextStyle(
-              //                                 fontSize: 20,
-              //                                 fontWeight: FontWeight.bold,
-              //                                 color: Colors.white,
-              //                                 // fontFamily: "Neucha"
-              //                               ),)
-              //                         ) ,
-              //                       ),
-              //                     );
-              //                   }
-              //                   ,
-              //                 ),
-              //               ),
-              //             ),
-              //             Container(
-              //               padding: EdgeInsets.fromLTRB(18, 10, 0, 0),
-              //               child: Row(
-              //                 mainAxisAlignment: MainAxisAlignment.start,
-              //                 children: [
-              //                   Text("Latest News: ",
-              //                     style: TextStyle(
-              //                       color: Colors.white,
-              //                       fontSize: 24,
-              //                       fontWeight: FontWeight.bold,
-              //
-              //
-              //                     ),),
-              //                 ],
-              //               ),
-              //             ),
-              //             isLoading ?
-              //             Container(
-              //                 height: MediaQuery.of(context).size.height - 400,
-              //                 child: Center(child: CircularProgressIndicator())) :
-              //
-              //             ListView.builder(
-              //                 physics: NeverScrollableScrollPhysics(),
-              //                 shrinkWrap: true,
-              //                 itemCount: newsModelList.length,
-              //
-              //                 itemBuilder: (context, index){
-              //                   try{
-              //                     return Container(
-              //                         margin: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-              //                         child: InkWell(
-              //                           onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>NewsView(newsModelList[index].newsUrl)));},
-              //                           child: Card(
-              //                               shape: RoundedRectangleBorder(
-              //                                 borderRadius: BorderRadius.circular(15),
-              //                               ),
-              //                               elevation: 7.0,
-              //                               child: Stack(
-              //                                 children: [
-              //                                   ClipRRect(
-              //                                       borderRadius: BorderRadius.circular(15),
-              //                                       child: Image.network(newsModelList[index].newsImg)
-              //                                   ),
-              //                                   Positioned(
-              //                                     left: 0,
-              //                                     right: 0,
-              //                                     bottom: 0,
-              //                                     child: Container(
-              //                                       padding: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-              //                                       decoration: BoxDecoration(
-              //                                           borderRadius: BorderRadius.circular(15),
-              //                                           gradient: LinearGradient(
-              //                                             colors: [Colors.black12.withOpacity(0),Colors.black],
-              //                                             begin: Alignment.topCenter,
-              //                                             end: Alignment.bottomCenter,
-              //
-              //                                           )
-              //                                       ),
-              //                                       child: Column(
-              //                                         crossAxisAlignment: CrossAxisAlignment.start,
-              //                                         children: [
-              //                                           Text(newsModelList[index].newsHead,
-              //                                             style: TextStyle(
-              //                                                 color: Colors.white,
-              //                                                 fontWeight: FontWeight.bold,
-              //                                                 fontSize: 18
-              //                                             ),),
-              //                                           Text(newsModelList[index].newsDesc.length > 50 ? "${newsModelList[index].newsDesc.substring(0,50)}......": newsModelList[index].newsDesc,
-              //                                             style: TextStyle(
-              //                                                 color: Colors.white,
-              //                                                 fontWeight: FontWeight.bold,
-              //                                                 fontSize: 11
-              //                                             ),),
-              //
-              //                                         ],
-              //                                       ),
-              //                                     ) ,
-              //
-              //                                   )
-              //                                 ],
-              //                               )
-              //                           ),
-              //                         )
-              //
-              //                     );
-              //                   }catch (e){return Container();}
-              //                 }),
-              //             Container(
-              //               padding: EdgeInsets.fromLTRB(0, 10, 0, 15),
-              //               child: ElevatedButton(onPressed: (){
-              //                 Navigator.push(context, MaterialPageRoute(builder: (context) => CategoryPage(query: "More News")));
-              //               }, child: Text("Show More"),
-              //
-              //               ),
-              //             )
-              //           ],
-              //         ),
-              //     ),
-              //   ),
-              //
-              // ),
+              Expanded(
+                flex: 3,
+                child: Container(
+                  child: SingleChildScrollView(
+                    child: Column(
+
+                        children: [
+                          Container(
+                            color: Colors.black12,
+                            padding: EdgeInsets.symmetric(vertical: 3),
+                            height: 50,
+                            child: Center(
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: navBarItem.length,
+                                itemBuilder: (context ,index ){
+                                  return InkWell(
+                                    onTap: (){
+                                      if (navBarItem[index]!= "Home")
+                                      {
+                                        Navigator.push(context, MaterialPageRoute(builder: (context) => CategoryPage(query: navBarItem[index])));
+                                      }
+                                    },
+                                    child: Container(
+
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(14),
+                                        color: Colors.blue.shade900,
+
+                                      ),
+                                      padding: EdgeInsets.symmetric(horizontal: 20),
+
+                                      margin: EdgeInsets.symmetric(horizontal: 7),
+                                      child: Center
+                                        (
+                                          child: Text(navBarItem[index],
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white,
+                                              // fontFamily: "Neucha"
+                                            ),)
+                                      ) ,
+                                    ),
+                                  );
+                                }
+                                ,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(18, 10, 0, 0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text("Latest News: ",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+
+
+                                  ),),
+                              ],
+                            ),
+                          ),
+                          isLoading ?
+                          Container(
+                              height: MediaQuery.of(context).size.height - 400,
+                              child: Center(child: CircularProgressIndicator())) :
+
+                          ListView.builder(
+                              physics: NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: newsModelList.length,
+
+                              itemBuilder: (context, index){
+                                try{
+                                  return Container(
+                                      margin: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                                      child: InkWell(
+                                        onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>NewsView(newsModelList[index].newsUrl)));},
+                                        child: Card(
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(15),
+                                            ),
+                                            elevation: 7.0,
+                                            child: Stack(
+                                              children: [
+                                                ClipRRect(
+                                                    borderRadius: BorderRadius.circular(15),
+                                                    child: Image.network(newsModelList[index].newsImg)
+                                                ),
+                                                Positioned(
+                                                  left: 0,
+                                                  right: 0,
+                                                  bottom: 0,
+                                                  child: Container(
+                                                    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                                                    decoration: BoxDecoration(
+                                                        borderRadius: BorderRadius.circular(15),
+                                                        gradient: LinearGradient(
+                                                          colors: [Colors.black12.withOpacity(0),Colors.black],
+                                                          begin: Alignment.topCenter,
+                                                          end: Alignment.bottomCenter,
+
+                                                        )
+                                                    ),
+                                                    child: Column(
+                                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                                      children: [
+                                                        Text(newsModelList[index].newsHead,
+                                                          style: TextStyle(
+                                                              color: Colors.white,
+                                                              fontWeight: FontWeight.bold,
+                                                              fontSize: 18
+                                                          ),),
+                                                        Text(newsModelList[index].newsDesc.length > 50 ? "${newsModelList[index].newsDesc.substring(0,50)}......": newsModelList[index].newsDesc,
+                                                          style: TextStyle(
+                                                              color: Colors.white,
+                                                              fontWeight: FontWeight.bold,
+                                                              fontSize: 11
+                                                          ),),
+
+                                                      ],
+                                                    ),
+                                                  ) ,
+
+                                                )
+                                              ],
+                                            )
+                                        ),
+                                      )
+
+                                  );
+                                }catch (e){return Container();}
+                              }),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(0, 10, 0, 15),
+                            child: ElevatedButton(onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (context) => CategoryPage(query: "More News")));
+                            }, child: Text("Show More"),
+
+                            ),
+                          )
+                        ],
+                      ),
+                  ),
+                ),
+
+              ),
               Expanded(
                 flex: 1,
                 child: Container(
